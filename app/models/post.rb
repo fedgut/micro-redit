1659ed8a2d3683f58a7fc5c9e3e7356bcd701f92
fedgut <fedgut@gmail.com>
2019-10-31 15:20:21 -0600

    Correct Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   validates :title,  presence: true, length: { maximum: 50 }
   validates :body, presence: true
-  belongs_to :user, foreign_key: :user_id
+  belongs_to :user, foreign_key: 'author_id'
 end
