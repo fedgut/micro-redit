bdaa9a7ef5d893735096d7ab4e805ab29eb1e7cb
fedgut <fedgut@gmail.com>
2019-10-31 15:56:26 -0600

    Correct Linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
-  validates :title,  presence: true, length: { maximum: 50 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true
   belongs_to :user, foreign_key: 'author_id'
   has_many :comments
